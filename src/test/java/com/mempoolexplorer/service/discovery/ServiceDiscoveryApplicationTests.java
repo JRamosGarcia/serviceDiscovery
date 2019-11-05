c073ca94938b5b16c9f725529f7850959327a0ca
kk <a@a.com>
2019-11-05 16:41:26 +0100

    Renombrada paquetería

--- src/test/java/com/mempoolexplorer/service/discovery/ServiceDiscoveryApplicationTests.java
+++ src/test/java/com/mempoolexplorer/service/discovery/ServiceDiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mempoolexplorer.serviceDiscovery;
+package com.mempoolexplorer.service.discovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
